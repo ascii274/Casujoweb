9740e38bde2dabbf9158acda638e976bc04a51e9
Joel Cabatay <ascii274@gmail.com>
2021-12-19 17:17:12 +0100

    Admin service created

---
+++ Casujoweb-admin/src/main/java/com/ascii274/casujoweb/admin/App.java
@@ -0,0 +1,13 @@
+package com.ascii274.casujoweb.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasujowebAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasujowebAdminApplication.class, args);
+	}
+
+}
---
+++ Casujoweb-admin/src/test/java/com/ascii274/casujoweb/admin/CasujowebAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ascii274.casujoweb.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasujowebAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
