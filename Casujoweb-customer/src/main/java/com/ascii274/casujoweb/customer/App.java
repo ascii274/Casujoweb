af686fe991ceb72abf7021c313fdbb7cef41e9ac
Joel Cabatay <ascii274@gmail.com>
2021-12-19 17:18:06 +0100

    Customer service created

---
+++ Casujoweb-customer/src/main/java/com/ascii274/casujoweb/customer/App.java
@@ -0,0 +1,13 @@
+package com.ascii274.casujoweb.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasujowebCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasujowebCustomerApplication.class, args);
+	}
+
+}
