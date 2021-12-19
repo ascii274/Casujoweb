6d18094bbf2d8227ab7c90b4092bb65bfd3a2a5f
Joel Cabatay <ascii274@gmail.com>
2021-12-19 17:15:20 +0100

    Login services created

---
+++ Casujoweb-login/src/main/java/com/ascii274/casujoweb/login/CasujowebLoginApplication.java
@@ -0,0 +1,13 @@
+package com.ascii274.casujoweb.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasujowebLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasujowebLoginApplication.class, args);
+	}
+
+}
---
+++ Casujoweb-login/src/test/java/com/ascii274/casujoweb/login/App.java
@@ -0,0 +1,13 @@
+package com.ascii274.casujoweb.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasujowebLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
